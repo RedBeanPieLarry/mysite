e1beaefee6eee61aaec2bab030c1268ab7ab50a4
RedBeanPieLarry <RedBeanPieLarry@gmail.com>
2014-03-26 08:51:03 +0800

    tutorial 5, add test cases to model.

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Poll(models.Model):
@@ -8,6 +10,9 @@ class Poll(models.Model):
     def __unicode__(self):  # Python 3: def __str__(self):
     	return self.question
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <  now
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
